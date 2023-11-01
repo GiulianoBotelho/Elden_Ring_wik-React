86a26f8deae09eac1c5a2184012eee4f89f63116
Devllans <135911807+GiulianoBotelho@users.noreply.github.com>
2023-11-01 15:12:13 +0000

    Update App.jsx and music.jsx

--- src/components/music.jsx
+++ src/components/music.jsx
@@ -1,18 +1,18 @@
-import { useEffect, useRef } from "react";
-import audioFile from "../assets/cutscene/main-sound.mp3";
+// import { useEffect, useRef } from "react";
+// import audioFile from "../assets/cutscene/main-sound.mp3";
 
-export default function Music() {
-  const audio = useRef();
-  useEffect(() => {
-    audio.current.volume = 0.5;
+// export default function Music() {
+//   const audio = useRef();
+//   useEffect(() => {
+//     audio.current.volume = 0.5;
 
-    audio.current.play();
-  }, []);
-  return (
-    <>
-      <div>
-        <audio ref={audio} src={audioFile} loop />
-      </div>
-    </>
-  );
-}
+//     audio.current.play();
+//   }, []);
+//   return (
+//     <>
+//       <div>
+//         <audio ref={audio} src={audioFile} loop />
+//       </div>
+//     </>
+//   );
+// }
