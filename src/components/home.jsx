3d846c1a600d5bf441d93d83c69db974ee020c27
GiulianoBotelho <giulianobotelho@outlook.com.br>
2023-11-24 18:37:43 -0300

    feat: home

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import { motion } from 'framer-motion'
 import styled from 'styled-components';
+import varre from '../assets/background/varre.jpg'
+import melina from '../assets/background/melina.jpg'
+import torrent from '../assets/background/torrent.jpg'
 export const Intro = styled(motion.section)`
   font-size: 1.5rem;
   width: 100%;
@@ -25,13 +28,13 @@ export const TitleHome = styled(motion.h1)`
   box-shadow: 1px, 1px, 1px, 1px;
   width: 99%;
   text-align: center;
-  font-size: 1rem;
+  font-size: 1.5rem;
   opacity: 1;
   cursor: pointer;
 `
 export const P = styled(motion.p)`
 color:aliceblue;
-font-size:1.2rem;
+font-size:1.5rem;
 text-align: center;
 width: 50%;
 margin-top: 20px;
@@ -44,6 +47,13 @@ export const CorTitulo = styled.article`
   width: 100%;
   text-align: center;
 `
+const SectionVideo = styled.section`
+display: flex;
+align-items: center;
+flex-wrap: wrap;
+justify-content: center;
+
+`
 
 export const Video = styled(motion.iframe)`
 margin-top: 30px;
@@ -54,6 +64,27 @@ height: 30vh;
     width: 70%;
     height: 30vh;
 }
+`
+
+const Titulos = styled.h2`
+font-size: 3rem;
+background-color: black;
+color: aliceblue;
+`
+const FundoTitulo = styled.div`
+width: 100%;
+background-color: black;
+display: flex;
+justify-content: center;
+`
+
+const HomeImg = styled(motion.img)`
+ width:70vw;
+ height: 70vh;
+ @media (max-width:500px){
+    width: 100%;
+    height: 20%;
+ }
 `
 export default function Home() {
     return (
@@ -86,6 +117,7 @@ export default function Home() {
                     the player character is a Tarnished, one of a group of exiles from the Lands Between who are summoned back after the Shattering.
                     The player must traverse the realm to repair the Elden Ring and become the Elden Lord.
                 </P>
+                <SectionVideo>
                 <Video
                     initial={{ opacity: 0, x: -100 }}
                     animate={{ opacity: 1, x: 0 }}
@@ -108,6 +140,48 @@ export default function Home() {
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                     allowFullScreen>
                 </Video>
+
+                </SectionVideo>
+                <FundoTitulo>
+                <Titulos>Entering the World</Titulos>
+                </FundoTitulo>
+                    <P>Elden Ring is an open-world action RPG developed by From Software. As a Tarnished, it’s your destiny to explore the Lands Between and become an Elden Lord.
+
+At the start of the game, you awaken in a tomb in the Lands Between, a place ruled by demigods following the shattering of the Elden Ring, the source of the Erdtree. The barrier between life and death no longer functions and people can no longer die.</P>
+                <figure>
+                 <HomeImg  initial={{ opacity: 0, x: -100 }}
+                    animate={{ opacity: 1, x: 0 }}
+                    transition={{ duration: 1, delay: 1.5 }}
+                     src={varre} alt="npc varre" />
+                </figure>
+                <P>After emerging in the initial area of Limgrave, you encounter Varré near the first Site of Grace. He informs you that your first destination should be Castle Stormveil. The light emanating from the Site of Grace points in the direction you should follow.
+
+Resist the temptation to head straight for the castle. While this direction offers some essential upgrades, you’ll want to delay arriving at the castle for a few hours. Instead, take the time to understand how things work and strengthen your character.</P>
+                <FundoTitulo>
+                <Titulos>Getting to know Melina</Titulos>
+                </FundoTitulo>
+                <figure>
+                    <HomeImg  initial={{ opacity: 0, x: -100 }}
+                    animate={{ opacity: 1, x: 0 }}
+                    transition={{ duration: 1, delay: 1.5 }} 
+                    src={melina} alt="npc melina e tarnished" />
+                </figure>
+                <P>
+                Shortly after, you encounter Melina, who visits you near the Ruins of Gatefront. She allows you to level up. This is also the point where you obtain the horse, Torrent, which aids you in exploring the region in a faster and easier manner.
+                </P>
+
+                <FundoTitulo>
+                <Titulos>Time to explore!</Titulos>
+                </FundoTitulo>
+                <figure>
+                    <HomeImg  initial={{ opacity: 0, x: -100 }}
+                    animate={{ opacity: 1, x: 0 }}
+                    transition={{ duration: 1, delay: 1.5 }}
+                     src={torrent} alt="cavalo torrent" />
+                </figure>
+                <P>
+                One of the biggest new features in Elden Ring is the inclusion of Torrent, a horse that the player can use for combat and exploration. Torrent proves to be a handy way to negotiate the challenging terrain of the Lands Between. The verticality of the map is facilitated by Torrent’s ability to double jump, and he can even launch himself high into the air by jumping on specific points scattered across the land. Riding Torrent greatly speeds up exploration and can lead players to normally inaccessible locations with its vertically oriented set of moves.
+                </P>
             </Intro>
         </>
     )
